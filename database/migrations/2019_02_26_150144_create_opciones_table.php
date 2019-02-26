94e6d4eb30d4ac37d48807c54a84779ded48583e
Jonatan <xmaldonado310x@gmail.com>
2019-02-26 15:37:37 -0600

    Migration para crear la tabla opcion en db

---
+++ database/migrations/2019_02_26_150144_create_opciones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpcionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('opcion', 25);
+            $table->integer('num_votos');
+            $table->enum('estado', ['Activo', 'Inactivo'])->default('Activo');
+            $table->timestamps();
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opciones');
+    }
+}
