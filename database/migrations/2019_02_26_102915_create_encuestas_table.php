f694251f34ccb6db5913e446adee69f8f436bc9c
Jonatan <xmaldonado310x@gmail.com>
2019-02-26 14:51:49 -0600

    Migracion para crear la tabla encuestas en la db

---
+++ database/migrations/2019_02_26_102915_create_encuestas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEncuestasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('encuestas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo', 50);
+            $table->string('descripcion', 150);
+            $table->enum('estado', ['Activo', 'Inactivo'])->default('Activo');
+            $table->timestamps();
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('encuestas');
+    }
+}
