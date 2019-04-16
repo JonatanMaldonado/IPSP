baeb25d527888650cf04c29f1b924645ae46840a
Jonatan Maldonado <jonatan.maldonado@nuestrodiario.com.gt>
2019-04-16 16:01:14 -0600

    Arreglo de nombre del modelo

--- app/Models/VotoUser.php
+++ app/Models/VotoUser.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Encuesta extends Model
+class VotoUser extends Model
 {
     protected $table = 'voto_users';
 
-    protected $primaryKey = 'idvoto_users';
+    protected $primaryKey = 'idvoto_user';
 
     protected $fillable = [
         'iduser',
